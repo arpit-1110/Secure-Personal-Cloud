d13cab414c450ba414e952c33f7c72ee32ed6224
Arpit Aggarwal <32244662+arpit-1110@users.noreply.github.com>
2018-11-15 21:44:42 +0530

    Modified linux_upload to encrypt the file

--- spc/linux_upload.py
+++ spc/linux_upload.py
@@ -2,7 +2,7 @@ import sys
 import requests
 import getpass
 import pickle
-
+from encrypt_decrypt import encrypt_file
 def upload():
 	client1 = requests.session()
 	url= "http://127.0.0.1:8000/accounts/login/"
@@ -25,16 +25,23 @@ def upload():
 		'csrfmiddlewaretoken':csrftoken
 	}
 	
-	client1.post(url,data=login_data)
-	csrftoken = client1.cookies['csrftoken']
+	q=client1.post(url,data=login_data)
 
+	if (q.content[100] != 115):
+		print("Invalid Request")
+		return False
+	csrftoken = client1.cookies['csrftoken']
+	password = getpass.getpass()
+	password = password.encode('utf-8')
+	key = hashlib.sha256(password).hexdigest()
+	encrypt_file(key[:16], file)
+	file += file + '.enc'
 	doc={
 		'document' : file
 
 	}
 
 	lols= {
-        '
 		'description' : desc,
 		'location' : add,
 		'submit': 'Upload',
