74273625f5e85a0fdecb3f599dd5547ee6d38776
arpitaggarwal <arpit.aggarwal1999@gmail.com>
2018-11-23 13:51:38 +0000

    Minor changes in sync

--- spc/sync.py
+++ spc/sync.py
@@ -5,6 +5,7 @@ import pickle
 import hashlib
 import os
 import wget
+import time
 
 pass_file = "pass.p"
 home_dir = os.getcwd()
@@ -67,258 +68,284 @@ with open("root_dir.p",'rb') as f:
 
 #download wala part ========================================================
 
-os.chdir(root_dir)
-# client = requests.session()
-currpath = ""
+url = "http://127.0.0.1:8000/api/v1/get_time_info/"
+r = client.get(url)
+time_info = r.json()
+# print(time_info)
 
-url="http://127.0.0.1:8000/api/v1/rootfinder/"
+if (int(time_info['allowed']) == 1) or (int(time_info['allowed']) == 0 and time.time() - float(time_info['time']) > 30):
 
-r = client.options(url)
-parent_id = r.json()["root"]
+	url = "http://127.0.0.1:8000/api/v1/update_time_info/"
+	data = { 'sync_allowed' : '0' , 'timestamp' : str(time.time())}
+	r = client.post(url,data = data)
 
-from en_de import AESde
-from en_de import DES3de
-from en_de import TWOde
+#sync starting ============================================================================
+	os.chdir(root_dir)
+	# client = requests.session()
+	currpath = ""
 
+	url="http://127.0.0.1:8000/api/v1/rootfinder/"
 
-def filedownload(path,name,infofile, passwd, option):
-	url = "http://127.0.0.1:8000/files/download/?name="+infofile
-	filename = wget.download(url,out=path)
-	if option == 'AES' :
-		key = hashlib.sha256(passwd.encode('utf-8')).digest()
-		AESde(key[:32], filename)
-	if option == 'DES3' :
-		key = hashlib.sha256(passwd.encode('utf-8')).digest()
-		DES3de(key[:16], filename)
-	if option == 'Twofish' :
-		TWOde(key, filename)
-	try:
-		# print('goes here')
-		os.remove(filename)
-	except:
-		pass
-	try:
-		if path == '':
-			os.rename(filename,name)
-		else:
-			os.rename(filename,path+"/"+name)
-	except:
-		# print("shit")
-		pass
+	r = client.options(url)
+	parent_id = r.json()["root"]
+
+	from en_de import AESde
+	from en_de import DES3de
+	from en_de import TWOde
 
 
-
-
-def recur_download(path,id, passwd, option):
-	url = "http://127.0.0.1:8000/api/v1/filedownload/"+str(id)+"/"
-	# print()
-	global stoc_for_all
-	global ctos_for_all
-	r = client.get(url)
-	# print(r)
-	infolist = r.json()
-
-	for info in infolist['info']:
+	def filedownload(path,name,infofile, passwd, option):
+		url = "http://127.0.0.1:8000/files/download/?name="+infofile
+		filename = wget.download(url,out=path)
+		if option == 'AES' :
+			key = hashlib.sha256(passwd.encode('utf-8')).digest()
+			AESde(key[:32], filename)
+		if option == 'DES3' :
+			key = hashlib.sha256(passwd.encode('utf-8')).digest()
+			DES3de(key[:16], filename)
+		if option == 'Twofish' :
+			TWOde(key, filename)
 		try:
-			dircontent = os.listdir(path)
+			# print('goes here')
+			os.remove(filename)
 		except:
-			dircontent = os.listdir()
-		# print("info dicti is :"+str(info))
-		name = info['name']
-		# name = name.split("/")[-1]
-		# print("naam hai :" + name)
-		# print(dircontent)
-
-		if (name in dircontent):
-			# print(dircontent)
+			pass
+		try:
 			if path == '':
-				iskifile = name
+				os.rename(filename,name)
 			else:
-				iskifile = path+"/"+name
-			if md5(iskifile) == info['md5sum']:
-				# print("haan same hai")
-				pass
-			else:
-				print("change ho gaya")
-
-				if stoc_for_all == True:
-					filedownload(path,name,info['file'], passwd, option)
-				elif ctos_for_all == True:
-					print("query is 2 kuchh nahi karna")
-				else:
-
-					q = int(input("files confict. You have 4 options: \
-											1: Server to your system for this file\
-											2: Your system to server for this file\
-											3: Server to your system for all files\
-											4: Your system to server for all file "))
-					if q == 1:
-						filedownload(path,name,info['file'], passwd, option)
-					elif q == 2:
-						# print("query is 2 kuchh nahi karna")
-						pass
-					elif q == 3:
-						filedownload(path,name,info['file'], passwd, option)
-						stoc_for_all = True
-					else:
-						# print("query is 2 kuchh nahi karna")
-						ctos_for_all = True
-
-
-		else:
-			filedownload(path,name,info['file'], passwd, option)
-
-
-	url = "http://127.0.0.1:8000/api/v1/folderlist/"+str(id)+"/"
-	r = client.get(url)
-	# print(r)
-	infolist = r.json()["folderlist"]
-	# print(infolist)
-
-	for info in infolist:
-		try:
-			dircontent = os.listdir(path)
+				os.rename(filename,path+"/"+name)
 		except:
-			dircontent = os.listdir()
-		if (info['name'] in dircontent):
-			name = info["name"]
-			folderid = info["id"]
-			if path =='':
-				folderpath = name
+			# print("shit")
+			pass
+
+
+
+
+	def recur_download(path,id, passwd, option):
+		url = "http://127.0.0.1:8000/api/v1/filedownload/"+str(id)+"/"
+		# print()
+		global stoc_for_all
+		global ctos_for_all
+		r = client.get(url)
+		# print(r)
+		infolist = r.json()
+
+		for info in infolist['info']:
+			try:
+				dircontent = os.listdir(path)
+			except:
+				dircontent = os.listdir()
+			# print("info dicti is :"+str(info))
+			name = info['name']
+			# name = name.split("/")[-1]
+			# print("naam hai :" + name)
+			# print(dircontent)
+
+			if (name in dircontent):
+				# print(dircontent)
+				if path == '':
+					iskifile = name
+				else:
+					iskifile = path+"/"+name
+				if md5(iskifile) == info['md5sum']:
+					# print("haan same hai")
+					pass
+				else:
+					print("change ho gaya")
+
+					if stoc_for_all == True:
+						filedownload(path,name,info['file'], passwd, option)
+					elif ctos_for_all == True:
+						print("query is 2 kuchh nahi karna")
+					else:
+
+						q = int(input("files confict. You have 4 options: \
+												1: Server to your system for this file\
+												2: Your system to server for this file\
+												3: Server to your system for all files\
+												4: Your system to server for all file "))
+						if q == 1:
+							filedownload(path,name,info['file'], passwd, option)
+						elif q == 2:
+							# print("query is 2 kuchh nahi karna")
+							pass
+						elif q == 3:
+							filedownload(path,name,info['file'], passwd, option)
+							stoc_for_all = True
+						else:
+							# print("query is 2 kuchh nahi karna")
+							ctos_for_all = True
+
+
 			else:
-				folderpath = path + "/" + name
-			# os.mkdir(folderpath)
-			# print(folderpath)
-			recur_download(folderpath,folderid, passwd, option)
-		else:
-			name = info["name"]
-			folderid = info["id"]
-			if path =='':
-				folderpath = name
+				filedownload(path,name,info['file'], passwd, option)
+
+
+		url = "http://127.0.0.1:8000/api/v1/folderlist/"+str(id)+"/"
+		r = client.get(url)
+		# print(r)
+		infolist = r.json()["folderlist"]
+		# print(infolist)
+
+		for info in infolist:
+			try:
+				dircontent = os.listdir(path)
+			except:
+				dircontent = os.listdir()
+			if (info['name'] in dircontent):
+				name = info["name"]
+				folderid = info["id"]
+				if path =='':
+					folderpath = name
+				else:
+					folderpath = path + "/" + name
+				# os.mkdir(folderpath)
+				# print(folderpath)
+				recur_download(folderpath,folderid, passwd, option)
 			else:
-				folderpath = path + "/" + name
-			os.mkdir(folderpath)
-			# print(folderpath)
-			recur_download(folderpath,folderid, passwd, option)
+				name = info["name"]
+				folderid = info["id"]
+				if path =='':
+					folderpath = name
+				else:
+					folderpath = path + "/" + name
+				os.mkdir(folderpath)
+				# print(folderpath)
+				recur_download(folderpath,folderid, passwd, option)
 
 
 
-#upload wala part ========================================================
+	#upload wala part ========================================================
 
 
-from enc import enc_upload
+	from enc import enc_upload
 
-def fileupload(path,name,parent_id, passwd, option):
-	# print(passwd)
-	# print(option)
-	loc = path+"/"+name
-	file_data = {
-	'name' : name,
-	'md5sum' : md5(loc),
-	}
-	file = enc_upload(path, name, passwd, option)
-	# print(name)
-	
-	# print(str(file))
-
-	upfiles = {
-		# 'name' : name,
-		'file' : file,
-	}
-	
-	# print(file_data['name'])
-	url = "http://127.0.0.1:8000/api/v1/uploadfile/"+str(parent_id)+"/"
-	r = client.post(url,data = file_data, files=upfiles,timeout = 1000000)
-	os.remove(loc+'.enc')
-	return r.json()['status']
-
-
-def createfolder(name,parent_id):
-	url = "http://127.0.0.1:8000/api/v1/createfolder/"+str(parent_id)+"/"
-
-	folder_data = {
+	def fileupload(path,name,parent_id, passwd, option):
+		# print(passwd)
+		# print(option)
+		loc = path+"/"+name
+		file_data = {
 		'name' : name,
-	}
-	r = client.post(url,data = folder_data)
+		'md5sum' : md5(loc),
+		}
+		file = enc_upload(path, name, passwd, option)
+		# print(name)
+		
+		# print(str(file))
 
-	# print(parent_id)
-	return r.json()["key"]
-
-
-
-
-def recur_upload(path,parent_id, passwd, option):
-	for f in listdir_nohidden(path):
-		if os.path.isfile(path+"/"+f) == True:
-			# print("goes here")
-			status = fileupload(path,f,parent_id, passwd, option)
-			if(status == "yes"):
-				print("uploading "+path+"/"+f+"......")
-		elif os.path.isdir(path+"/"+f) == True:
-			# print("goes here also")
-			key = createfolder(f,parent_id)
-			path2 = path + "/" + f
-			recur_upload(path2,key, passwd, option)
-
-url="http://127.0.0.1:8000/api/v1/rootfinder/"
-
-r = client.options(url)
-parent_id = r.json()["root"]
+		upfiles = {
+			# 'name' : name,
+			'file' : file,
+		}
+		
+		# print(file_data['name'])
+		url = "http://127.0.0.1:8000/api/v1/uploadfile/"+str(parent_id)+"/"
+		r = client.post(url,data = file_data, files=upfiles,timeout = 1000000)
+		os.remove(loc+'.enc')
+		return r.json()['status']
 
 
-# name = path.split("/")[-1]
+	def createfolder(name,parent_id):
+		url = "http://127.0.0.1:8000/api/v1/createfolder/"+str(parent_id)+"/"
 
-# new_parent_id = createfolder(name,parent_id) #extract name from path
-import shutil
+		folder_data = {
+			'name' : name,
+		}
+		r = client.post(url,data = folder_data)
 
-def init() :
-	global parent_id
-	global home_dir
-	global root_dir
-	# print(root_dir)
-	os.chdir(home_dir)
+		# print(parent_id)
+		return r.json()["key"]
+
+
+
+
+	def recur_upload(path,parent_id, passwd, option):
+		for f in listdir_nohidden(path):
+			if os.path.isfile(path+"/"+f) == True:
+				# print("goes here")
+				status = fileupload(path,f,parent_id, passwd, option)
+				if(status == "yes"):
+					print("uploading "+path+"/"+f+"......")
+			elif os.path.isdir(path+"/"+f) == True:
+				# print("goes here also")
+				key = createfolder(f,parent_id)
+				path2 = path + "/" + f
+				recur_upload(path2,key, passwd, option)
+
+	url="http://127.0.0.1:8000/api/v1/rootfinder/"
+
+	r = client.options(url)
+	parent_id = r.json()["root"]
+
+
+	# name = path.split("/")[-1]
+
+	# new_parent_id = createfolder(name,parent_id) #extract name from path
+	import shutil
+
+	def init() :
+		global parent_id
+		global home_dir
+		global root_dir
+		# print(root_dir)
+		os.chdir(home_dir)
+		with open(pass_file, 'rb') as f :
+			data = pickle.load(f)
+		if data['md5'] == 'null' :
+			passwd = take_pass()
+			with open(pass_file, 'wb') as f :
+				pickle.dump({'md5': passwd, 'option' : data['option']}, f)
+			# return passwd	
+		elif data['option'] == 'null' :
+			op = take_option()
+			with open(pass_file, 'wb') as f :
+				pickle.dump({'md5': data['md5'], 'option' : op}, f)
+			# return passwd
+		else :
+			if len(sys.argv) > 1 :
+				with open(pass_file, 'rb') as f :
+					data = pickle.load(f)
+					prev_passwd = data['md5']
+					prev_option = data['option']
+				if 'o' in sys.argv[1] :
+					op = take_option()
+					with open(pass_file, 'wb') as f :
+						pickle.dump({'md5': data['md5'], 'option' : op}, f)
+				if 'c' in sys.argv[1] :
+					passwd = take_pass()
+					with open(pass_file, 'wb') as f :
+						pickle.dump({'md5' : passwd, 'option' : data['option']}, f)
+				with open(pass_file, 'rb') as f :
+					data = pickle.load(f)
+					new_passwd = data['md5']
+					new_option = data['option']
+				# os.chdir(root_dir)
+				recur_upload(root_dir,parent_id, new_passwd, new_option)
+
+	init()
+
 	with open(pass_file, 'rb') as f :
 		data = pickle.load(f)
-	if data['md5'] == 'null' :
-		passwd = take_pass()
-		with open(pass_file, 'wb') as f :
-			pickle.dump({'md5': passwd, 'option' : data['option']}, f)
-		# return passwd	
-	elif data['option'] == 'null' :
-		op = take_option()
-		with open(pass_file, 'wb') as f :
-			pickle.dump({'md5': data['md5'], 'option' : op}, f)
-		# return passwd
-	else :
-		if len(sys.argv) > 1 :
-			with open(pass_file, 'rb') as f :
-				data = pickle.load(f)
-				prev_passwd = data['md5']
-				prev_option = data['option']
-			if 'o' in sys.argv[1] :
-				op = take_option()
-				with open(pass_file, 'wb') as f :
-					pickle.dump({'md5': data['md5'], 'option' : op}, f)
-			if 'c' in sys.argv[1] :
-				passwd = take_pass()
-				with open(pass_file, 'wb') as f :
-					pickle.dump({'md5' : passwd, 'option' : data['option']}, f)
-			with open(pass_file, 'rb') as f :
-				data = pickle.load(f)
-				new_passwd = data['md5']
-				new_option = data['option']
-			# os.chdir(root_dir)
-			recur_upload(root_dir,parent_id, new_passwd, new_option)
+	file_passwd = data['md5']
+	option = data['option']
+	os.chdir(root_dir)
+	import time
+	recur_download(currpath,parent_id, file_passwd, option)
+	time.sleep(0.01)
+	recur_upload(root_dir,parent_id, file_passwd, option)
 
-init()
+	print("sync done")
 
-with open(pass_file, 'rb') as f :
-	data = pickle.load(f)
-file_passwd = data['md5']
-option = data['option']
-os.chdir(root_dir)
-import time
-recur_download(currpath,parent_id, file_passwd, option)
-time.sleep(0.01)
-recur_upload(root_dir,parent_id, file_passwd, option)
+	#sync done ====================================================
+	url = "http://127.0.0.1:8000/api/v1/update_time_info/"
+
+	data = { 'sync_allowed' : '1' , 'timestamp' : str(time.time())}
+	r = client.post(url,data = data)
+	print(r)
+
+
+else:
+	print("Another sync is going on, please try again later")
+	exit()
 
